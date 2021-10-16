5679b259d22d8edaab463f6c7267661e4ca5fb71
Nikzero6 <nikzerosub@gmail.com>
2021-10-16 10:54:45 +0530

    react-bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-const App = () => <div>hello</div>;
+const App = () => (
+  <>
+    <Header />
+    <main style={{ minHeight: "93vh" }}></main>
+    <Footer />
+  </>
+);
 
 export default App;
