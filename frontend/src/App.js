6b9e36bee98c1e7ff15a32d158b901760d87122e
Nikzero6 <nikzerosub@gmail.com>
2021-10-16 12:55:38 +0530

    notes renedring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter, Route } from "react-router-dom";
+import Mynotes from "./screens/MyNotes/Mynotes";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <main style={{ minHeight: "93vh" }}></main>
+    <main>
+      <Route path="/" component={LandingPage} exact />
+      <Route path="/mynotes" component={Mynotes} exact />
+    </main>
     <Footer />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Notes-zipper</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <Link to="/mynotes">My Notes</Link>
+            </Nav.Link>
+            <NavDropdown title="Nikhil Rai" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">My Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
